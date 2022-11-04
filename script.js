c3fcbd37f16576470d76a08983bc4ec8d5a5235f
gurudhanush101 <111291068+gurudhanush101@users.noreply.github.com>
2022-11-04 10:22:02 +0530

    score update

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number!';
-// console.log(document.querySelector('.message').textContent);
 
-
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 210;
-// console.log(document.querySelector('.guess').value);
 let secret = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
@@ -53,7 +45,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
         document.querySelector('body').style.background = '#60b347';
         document.querySelector('.number').style.width = '30rem';
-        displayNumber(score);
+        displayNumber(secret);
 
         if (score > highscore) {
             highscore = score;
